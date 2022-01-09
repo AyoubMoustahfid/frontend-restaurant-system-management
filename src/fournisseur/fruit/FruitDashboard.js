3fe817171641a88c2f78cd9ca4116a83a422884b
Ayoub Moustahfid <moustahfidayoub12@gmail.com>
2022-01-09 22:41:20 +0100

    push new update front end to deploy in heroku

--- src/fournisseur/fruit/FruitDashboard.js
+++ src/fournisseur/fruit/FruitDashboard.js
@@ -9,9 +9,7 @@ import {getFournisseurById, getAllFruitByFournisseur, deleteFruit} from "./../Ap
 
 import { Line } from 'react-chartjs-2';
 
-import {icon_quantity_chart, icon_product_chart} from "./../../Constants/icons"
-// defaults.global.tooltips.enabled = false
-// defaults.global.legend.position = 'bottom'
+
 
 const PageSize = 3;
 function FruitDashboard() {
@@ -168,8 +166,7 @@ function FruitDashboard() {
     const array = []
     const valeur = []
 
-    console.log(elements && elements.forEach(item => array.push(item.name)))
-    console.log(elements && elements.forEach(item => valeur.push(item.package)))
+
     return (
         <Layout >
             <div className="row px-3">
@@ -366,7 +363,7 @@ function FruitDashboard() {
                                                 </div>
                                             </div>
 
-                                            <button className="btn btn-danger" onClick={() => deleteFruit(item._id, user._id, token)}>
+                                            <button className="btn btn-danger" onClick={() => deleteFruit(item._id, user._id, token).then(res => window.location.reload())}>
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-trash mx-2" viewBox="0 0 16 16">
                                                     <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                                                     <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
